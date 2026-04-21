1520ff405c0941b2a7d2f00d384fc505d695adcc
Likhil022 <likhilkosuru123@gmail.com>
2026-04-21 23:55:45 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,12 @@
+#String
+
+s = "AI Enginner in three months"
+print(s)
+print(s.lower())
+print(s.capitalize())
+print(s.upper())
+print(s.upper().isupper())
+print(len(s))
+print(s.index("i"))
+print(s.replace("s","z"))
+

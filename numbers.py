e92ad6ab684d93d0c2c97461f63b705f342da82a
Likhil022 <likhilkosuru123@gmail.com>
2026-04-21 23:54:33 +0530

    numbers

---
+++ numbers.py
@@ -0,0 +1,20 @@
+from math import *
+
+#number
+#functions - max, min, round, abs, pow
+
+print(1)
+print(-1)
+print(-2.0908)
+print(3 * (5+1))
+print(max(4,5))
+print(abs(-10))
+print(min(4,5))
+print(round(6.678))
+print(pow(4,3))
+
+
+#from math import
+print(floor(3.4))
+print(ceil(3.4))
+print(sqrt(36))
